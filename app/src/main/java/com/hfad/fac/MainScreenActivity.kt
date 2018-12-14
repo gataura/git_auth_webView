adeaf8edbbdb85d74b97f553a01d2eeb30ac0d67
salikh <k612nn@gmail.com>
2018-12-14 16:49:56 +0300

    Initial commit

---
+++ app/src/main/java/com/hfad/fac/MainScreenActivity.kt
@@ -0,0 +1,61 @@
+package com.hfad.fac
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainScreenActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+
+        webView = find(R.id.webView)
+
+        configureWebview()
+
+        webView.loadUrl("https://github.com/")
+        
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, MainScreenActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun configureWebview() {
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+            @RequiresApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                view.loadUrl(request.url.toString())
+                return true
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+}
